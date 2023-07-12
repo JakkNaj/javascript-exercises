79e2c6e452ac6f3daa478ad7b5d9912b1f51af44
Jakub Najman <jakub@najman.cz>
2023-07-12 23:07:16 +0200

    exercises 01-04 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, repTimes) {
+    if (repTimes < 0) return "ERROR"
+    let outcome = "";
+    for (let i = 0; i < repTimes; i++){
+        outcome += str
+    }
+    return outcome;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr = str.split("")
+    let reverseArr = splitStr.reverse()
+    return reverseArr.join("")
 };
 
 // Do not edit below this line
