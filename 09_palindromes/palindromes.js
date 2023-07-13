169cbe7e7ad9a236288243622391b6a17c5e840e
Jakub Najman <jakub@najman.cz>
2023-07-13 10:51:07 +0200

    js exercises 09-12 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase() //remove whitespaces using regex, the convert to lowerCase
+    let reverseStr = str.split('').reverse().join(''); //reverse the string
+    return str === reverseStr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let i = 0;
+    let oldestAge = 0;
+    let oldestIndex = -1;
+    while (i < array.length){
+        if (!array[i].yearOfDeath){
+            if (new Date().getFullYear() - array[i].yearOfBirth  > oldestAge){
+                oldestAge = new Date().getFullYear() - array[i].yearOfBirth;
+                oldestIndex = i;
+            }
+        } else if (array[i].yearOfDeath - array[i].yearOfBirth > oldestAge){
+            oldestAge = array[i].yearOfDeath - array[i].yearOfBirth;
+            oldestIndex = i;
+        }
+        i++;
+    }
+    return array[oldestIndex];
 };
 
 // Do not edit below this line
